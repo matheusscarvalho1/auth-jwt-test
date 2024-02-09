b647a497fb18dea1ac02379bc90c5ba4f45f9d34
Matheus de Souza Carvalho <matheus.carvalho@ginco.local>
2024-02-09 10:39:33 -0400

    Fix: Ajustando alguns dados do servidor e criando variáveis de ambiente

---
+++ src/env/index.ts
@@ -0,0 +1,9 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+const envSchema = z.object({
+    JWT_SECRET: z.string(),
+    })
+    
+export const env = envSchema.parse(process.env)
+
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import fastify from "fastify";
 import routes from "./routes/routes";
 import fastifyJwt from "@fastify/jwt";
+import { env } from './env'
 
 const app = fastify();
 
 // Configurando o segredo para o JWT
-const JWT_SECRET: string = "segredo_do_token";
+const JWT_SECRET: string = env.JWT_SECRET;
 
 // Registrando o plugin @fastify/jwt com a chave secreta
 app.register(fastifyJwt, {
@@ -13,14 +14,18 @@ app.register(fastifyJwt, {
 });
 
 app.get("/", async () => {
-  return "Hello World!";
+  return "Rotas disponíveis para o teste do token: [POST]/api/login, [GET]/api/protected, [GET]/api/test-token";
 });
 
 // Registrando suas rotas
-app.register(routes, { prefix: "app" });
+app.register(routes, { prefix: "api" });
 
 // Iniciando o servidor
-app.listen({ port: 3000, host: "0.0.0.0"}
- ).then(() => {
-  console.log("HTTP Server running on http://localhost:3000");
-});
+app.listen({ port: 3000, host: "localhost" })
+  .then(() => {
+    console.log(`HTTP Server running on http://localhost:3000`);
+  })
+  .catch(error => {
+    console.error("Error starting server:", error);
+    process.exit(1);
+  });
